0320651bd39d05b5c2e19dd191fda741d55d7f06
Loris Matteri <loris.matteri93@gmail.com>
2020-12-21 18:21:43 +0100

    Milestone 2 completato

--- Milestone 2/assets/js/main.js
+++ Milestone 2/assets/js/main.js
@@ -4,11 +4,12 @@ let app = new Vue ({
         userImage: './assets/img/avatar_io.jpg',
         send: 'inviato',
         rec: 'ricevuto',
+        activeUser: 0,
         contacts: [
             {
                 name: 'Michele',
                 avatar: './assets/img/avatar_1.jpg',
-                visible: false,
+                visible: true,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
@@ -30,7 +31,7 @@ let app = new Vue ({
             {
                 name: 'Fabio',
                 avatar: './assets/img/avatar_2.jpg',
-                visible: false,
+                visible: true,
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
@@ -53,32 +54,48 @@ let app = new Vue ({
             {
                 name: 'Samuele',
                 avatar: './assets/img/avatar_3.jpg',
-                visible: false,
+                visible: true,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
-                        text: 'Ciao come stai?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '20/03/2020 16:30:55',
-                        text: 'Bene grazie! Stasera ci vediamo?',
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
                     }
                 ],
                 
-            }
+            },
+            {
+                name: 'Luisa',
+                avatar: './assets/img/avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
         ]
     },
     methods: {
-        selected: function(){
-            this.visible = true;
-            console.log(this.visible);
+        selected: function(index){
+            this.activeUser = index;
         }
     }
 })
